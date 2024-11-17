85d4aa57e521f3dcf2a199bbec6e6d0710358c2b
vietdev112004 <vanvietpham453@gmail.com>
2024-11-17 11:20:23 +0700

    ex add

--- connectDB.php
+++ connectDB.php
@@ -13,4 +13,4 @@ $conn = new mysqli($host, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Kết nối thất bại: " . $conn->connect_error);
 }
-// echo "Kết nối thành công!";
+echo "Kết nối thành công!";
